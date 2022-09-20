6a96df6468a03f7c893982e471cb53c1013c326b
Julipoli99 <Polimenijulian9@gmail.com>
2022-09-20 15:51:39 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Hello World!");
+
+            Persona p = new Persona();
+
+            p.setNombre("Julian");
+            p.setApellido("Polimeni");
+
+            Console.WriteLine(p.nombreCompleto());
+        }
+    }
+
+    class Persona
+    {
+        private string nombre;
+        private string apellido;
+
+        public void setNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public void setApellido(string apellido)
+        {
+            this.apellido = apellido;
+        }
+
+        private string getNombre()
+        {
+            return this.nombre;
+        }
+
+        private string getApellido()
+        {
+            return this.apellido;
+        }
+
+
+        public string nombreCompleto()
+        {
+            return getNombre() + " " + getApellido();
+        }
+    }
+}
